3cd01d85b1abeaa711742435c3b43c3cb3b2524d
Kendry Vidal <kendryvidalguevera@gmail.com>
2023-11-16 16:17:32 -0500

    ready delete subjects and delete syllabus

---
+++ app/controllers/delete_syllabus.py
@@ -0,0 +1,29 @@
+from database.db import delete
+
+
+R = '\033[31m'  # Red
+RS = '\033[39m'  # Reset
+
+id_param_name = 'syllabus_id'
+
+def main(event):
+    
+    # Header
+    try:
+        params = event['params']
+
+        if id_param_name in params:
+            params['id'] = params.pop(id_param_name)
+
+    except KeyError as e:
+        return f"{R}* This method requires the parameters.{e}{RS}"
+    
+    # Body
+    result = {'status': False, 'row_count': 0}
+
+    result['status'] = bool(delete('syllabus', params))
+    
+    if result['status']:
+        result['row_count'] = 1
+    # Response
+    return {'status': bool(result), 'data': result}
