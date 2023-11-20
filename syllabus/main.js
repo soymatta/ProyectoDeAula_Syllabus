494fb9b116fa18b48a84cc00d96199b442f2576a
Ellery Ricaurte <elleryrica12@gmail.com>
2023-11-20 18:20:45 -0500

    Editor.html

--- syllabus/main.js
+++
@@ -1,8 +0,0 @@
-// main.js
-const showdown = require('showdown');
-const converter = new showdown.Converter();
-const text = '# Hello, Markdown!';
-const html = converter.makeHtml(text);
-console.log(html);
-
-
