0bcfa989b3a80b232b1c8d1ebd6e59918fbc7815
Yassed Matta <yassed883@gmail.com>
2023-11-21 16:32:44 -0500

    Agregando _data faltantes

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ from syllabus.db.db import app, db
 
 # Creación de tablas
 
-    # Tablas Independientes
+# Tablas Independientes
 from syllabus.models.subjects_model import Subjects
 from syllabus.models.faculties_model import Faculties
-    # Tablas Dependientes
+
+# Tablas Dependientes
 from syllabus.models.users_model import Users
 from syllabus.models.syllabi_model import Syllabi
 from syllabus.models.versions_model import Versions
 from syllabus.models.evaluations_model import Evaluations
 from syllabus.models.contentsAndStrategies_model import ContentsAndStrategies
-    # Tablas Intermedias
+
+# Tablas Intermedias
 from syllabus.models.users_subjects_model import UsersSubjects
 from syllabus.models.faculties_subjects_model import FacultiesSubjects
 from syllabus.models.users_syllabi_model import UsersSyllabi
@@ -39,97 +41,157 @@ app.register_blueprint(faculties_subjects_routes, url_prefix="/faculties_subject
 
 # Routes - Template Rendering
 
+
 # Menu / Homes
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def menu():
-    if 'user_id' in session:
-        user_id = session['user_id']
+    if "user_id" in session:
+        user_id = session["user_id"]
         user = Users.query.get(user_id)
 
         # Consulta de las materias por usuario
-        user_subjects = db.session.query(Subjects).join(UsersSubjects).filter(UsersSubjects.users_id == user_id).all()
-        data = [{'id': subject.id, 'name': subject.name} for subject in user_subjects]
+        user_subjects = (
+            db.session.query(Subjects)
+            .join(UsersSubjects)
+            .filter(UsersSubjects.users_id == user_id)
+            .all()
+        )
+        data = [{"id": subject.id, "name": subject.name} for subject in user_subjects]
 
-        return render_template('/menu.html', username = user.name, data = data)
-    else: return redirect(url_for('login'))
+        return render_template("/menu.html", username=user.name, data=data)
+    else:
+        return redirect(url_for("login"))
 
-# Login 
-@app.route('/login', methods=['GET', 'POST'])
+
+# Login
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if 'user_id' in session:
-        return redirect(url_for('menu'))
-    else: 
-        if request.method == 'POST':
-            email = request.form['emailInput']
-            password = request.form['passwordInput']
+    if "user_id" in session:
+        return redirect(url_for("menu"))
+    else:
+        if request.method == "POST":
+            email = request.form["emailInput"]
+            password = request.form["passwordInput"]
 
             user = Users.query.filter_by(email=email, password=password).first()
 
             if user:
-                session['user_id'] = user.id
-                print('Inicio de sesión exitoso')
-                return redirect(url_for('menu'))
+                session["user_id"] = user.id
+                print("Inicio de sesión exitoso")
+                return redirect(url_for("menu"))
             else:
-                print('Credenciales incorrectas')
-                return redirect(url_for('login'))
+                print("Credenciales incorrectas")
+                return redirect(url_for("login"))
 
-    return render_template('login.html')
+    return render_template("login.html")
+
 
 # Admin option panel
-@app.route('/admin/options', methods=['GET'])
+@app.route("/admin/options", methods=["GET"])
 def admin():
     # Si no hay sesión iniciada va a login
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    if "user_id" not in session:
+        return redirect(url_for("login"))
 
-    user_id = session['user_id']
+    user_id = session["user_id"]
     user = Users.query.get(user_id)
 
-    if user.role == 'admin':
-        return render_template('/adminPanel.html')
+    if user.role == "admin":
+        return render_template("/adminPanel.html")
     else:
-        return render_template('/adminDenegado.html')
+        return render_template("/adminDenegado.html")
+
 
 # Editor online
-@app.route('/editor/<int:syllabus_id>', methods=['GET'])
+@app.route("/editor/<int:syllabus_id>", methods=["GET"])
 def editor_by_id(syllabus_id):
-    if 'user_id' in session:
-
+    if "user_id" in session:
         syllabus = db.session.query(Syllabi).filter(Syllabi.id == syllabus_id).first()
-        evaluations = db.session.query(Evaluations).filter(Evaluations.syllabus_id == syllabus_id).first()
+
+        evaluations = (
+            db.session.query(Evaluations)
+            .filter(Evaluations.syllabus_id == syllabus_id)
+            .first()
+        )
+
+        faculty = (
+            db.session.query(Faculties)
+            .filter(Faculties.id == syllabus.faculty_id)
+            .first()
+        )
+        subject = (
+            db.session.query(Subjects)
+            .filter(Subjects.id == syllabus.subject_id)
+            .first()
+        )
+
+        versions = (
+            db.session.query(Versions).filter(Versions.syllabus_id == syllabus_id).all()
+        )
 
         syllabus_data = {
-                'id': syllabus.id,
-                'date': syllabus.date,
-                'program': syllabus.program,
-                'cycle': syllabus.cycle,
-                'justification': syllabus.justification,
-                'competences': syllabus.competences,
-                'learning_results': syllabus.learning_results,
-                'methodology': syllabus.methodology,
-                'faculty_id': syllabus.faculty_id,
-                'subjects_id': syllabus.faculty_id,
-            }
+            "id": syllabus.id,
+            "date": syllabus.date,
+            "program": syllabus.program,
+            "cycle": syllabus.cycle,
+            "justification": syllabus.justification,
+            "competences": syllabus.competences,
+            "learning_results": syllabus.learning_results,
+            "methodology": syllabus.methodology,
+            "faculty_id": syllabus.faculty_id,
+            "subjects_id": syllabus.subject_id,
+        }
+
+        faculty_data = {"id": faculty.id, "name": faculty.name}
+
+        subject_data = {
+            "name": subject.name,
+            "modality": subject.modality,
+            "type": subject.type,
+            "credits": subject.credits,
+            "semester": subject.semester,
+            "bibliography": subject.bibliography,
+        }
 
         evaluations_data = {
-                'first_percentage': evaluations.first_percentage,
-                'description_first_percentage': evaluations.description_first_percentage,
-                'second_percentage': evaluations.second_percentage,
-                'description_second_percentage': evaluations.description_second_percentage,
-                'third_percentage': evaluations.third_percentage,
-                'description_third_percentage': evaluations.description_third_percentage
-            }
+            "first_percentage": evaluations.first_percentage,
+            "description_first_percentage": evaluations.description_first_percentage,
+            "second_percentage": evaluations.second_percentage,
+            "description_second_percentage": evaluations.description_second_percentage,
+            "third_percentage": evaluations.third_percentage,
+            "description_third_percentage": evaluations.description_third_percentage,
+        }
 
-        return render_template('/editor.html', syllabus_data = syllabus_data, evaluations_data = evaluations_data)
+        versions_data = [
+            {
+                "id": version.id,
+                "update_date": version.update_date,
+                "description": version.description,
+                "user_id": version.user_id,
+                "syllabus_id": version.syllabus_id,
+            }
+            for version in versions
+        ]
+
+        return render_template(
+            "/editor.html",
+            syllabus_data=syllabus_data,
+            evaluations_data=evaluations_data,
+            faculty_data=faculty_data,
+            subject_data=subject_data,
+            versions_data=versions_data,
+        )
     else:
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
+
 
 # Cerrar sesión
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    session.pop('user_id', None)
-    print('Cierre de sesión exitoso')
-    return redirect(url_for('login'))
+    session.pop("user_id", None)
+    print("Cierre de sesión exitoso")
+    return redirect(url_for("login"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- syllabus/models/versions_model.py
+++ syllabus/models/versions_model.py
@@ -1,24 +1,26 @@
 from ..db.db import ma, db, app
 from sqlalchemy import DateTime
-from datetime import datetime 
+from datetime import datetime
+
 
 class Versions(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     update_date = db.Column(DateTime, nullable=False)
     description = db.Column(db.String(500))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    syllabi_id = db.Column(db.Integer, db.ForeignKey('syllabi.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    syllabus_id = db.Column(db.Integer, db.ForeignKey("syllabi.id"), nullable=False)
 
-    def __init__(self, update_date, description, owner, user_id, syllabi_id):
+    def __init__(self, update_date, description, user_id, syllabus_id):
         self.update_date = update_date
         self.description = description
-        self.owner = owner
         self.user_id = user_id
-        self.syllabi_id = syllabi_id
+        self.syllabus_id = syllabus_id
+
 
 class VersionsSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Versions
+
 
 version_schema = VersionsSchema()
 versions_schema = VersionsSchema(many=True)
