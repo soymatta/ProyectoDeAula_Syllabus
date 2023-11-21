e0d7cbb13904add1e3acec5e8253622f31ddb5db
Ellery Ricaurte <elleryrica12@gmail.com>
2023-11-21 17:40:23 -0500

    Adding dataBase to OnlineEditor

--- syllabus/static/jss/markdown.js
+++ syllabus/static/jss/markdown.js
@@ -1,15 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  JustificationRun();
+  competencesRun();
+  learningsRun();
+  methodologyRun();
+  bibliographyRun();
+  firstEvaluationRun();
+  secondEvaluationRun();
+  thirdEvaluationRun();
+});
+
 function JustificationRun() {
   var text = document.getElementById('JustificationText').value,
       target = document.getElementById('justification'),
       converter = new showdown.Converter();
 
-  // Convertir el texto de Markdown a HTML
   var html = converter.makeHtml(text);
-
-  // Procesar los bloques de Markdown para manejar mejor los saltos de línea
   html = processMarkdownLineBreaks(html);
-
-  // Actualizar el contenido del div de destino
   target.innerHTML = html;
 }
 
@@ -18,13 +24,8 @@ function competencesRun() {
       target = document.getElementById('competences'),
       converter = new showdown.Converter();
 
-  // Convertir el texto de Markdown a HTML
   var html = converter.makeHtml(text);
-
-  // Procesar los bloques de Markdown para manejar mejor los saltos de línea
   html = processMarkdownLineBreaks(html);
-
-  // Actualizar el contenido del div de destino
   target.innerHTML = html;
 }
 
@@ -33,13 +34,8 @@ function learningsRun() {
       target = document.getElementById('learning_results'),
       converter = new showdown.Converter();
 
-  // Convertir el texto de Markdown a HTML
   var html = converter.makeHtml(text);
-
-  // Procesar los bloques de Markdown para manejar mejor los saltos de línea
   html = processMarkdownLineBreaks(html);
-
-  // Actualizar el contenido del div de destino
   target.innerHTML = html;
 }
 
@@ -48,13 +44,8 @@ function methodologyRun() {
       target = document.getElementById('methodology'),
       converter = new showdown.Converter();
 
-  // Convertir el texto de Markdown a HTML
   var html = converter.makeHtml(text);
-
-  // Procesar los bloques de Markdown para manejar mejor los saltos de línea
   html = processMarkdownLineBreaks(html);
-
-  // Actualizar el contenido del div de destino
   target.innerHTML = html;
 }
 
@@ -63,30 +54,49 @@ function bibliographyRun() {
       target = document.getElementById('bibliography'),
       converter = new showdown.Converter();
 
-  // Convertir el texto de Markdown a HTML
   var html = converter.makeHtml(text);
-
-  // Procesar los bloques de Markdown para manejar mejor los saltos de línea
   html = processMarkdownLineBreaks(html);
+  target.innerHTML = html;
+}
 
-  // Actualizar el contenido del div de destino
+function firstEvaluationRun(){
+  var text = document.getElementById('firstEvaluationText').value,
+      target = document.getElementById('firstEvaluation'),
+      converter = new showdown.Converter();
+
+  var html = converter.makeHtml(text);
+  html = processMarkdownLineBreaks(html);
+  target.innerHTML = html;
+}
+
+function secondEvaluationRun(){
+  var text = document.getElementById('secondEvaluationText').value,
+      target = document.getElementById('secondEvaluation'),
+      converter = new showdown.Converter();
+
+  var html = converter.makeHtml(text);
+  html = processMarkdownLineBreaks(html);
+  target.innerHTML = html;
+}
+
+function thirdEvaluationRun(){
+  var text = document.getElementById('thirdEvaluationText').value,
+      target = document.getElementById('thirdEvaluation'),
+      converter = new showdown.Converter();
+
+  var html = converter.makeHtml(text);
+  html = processMarkdownLineBreaks(html);
   target.innerHTML = html;
 }
 
 function processMarkdownLineBreaks(html) {
-  // Dividir el HTML en bloques de Markdown
   var blocks = html.split(/<\/?(?:div|p)>/);
-
-  // Filtrar y agregar <br> según sea necesario
   var processedBlocks = blocks.map(function (block) {
       return block.trim() ? processMarkdownLineBreak(block) + '<br>' : '';
   });
-
-  // Unir los bloques procesados nuevamente en HTML
   return processedBlocks.join('');
 }
 
 function processMarkdownLineBreak(block) {
-  // Procesar los saltos de línea en bloques de Markdown
   return block.replace(/\n/g, '<br>');
 }
