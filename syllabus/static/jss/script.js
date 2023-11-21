9738fc021851aca1c04bdca0292b2f727ad52555
Yassed Matta <yassed883@gmail.com>
2023-11-21 01:49:52 -0500

    Agregando funcionalidades al frontend

--- syllabus/static/jss/script.js
+++ syllabus/static/jss/script.js
@@ -1,18 +1,1 @@
 console.log("Prueba JS");
-
-document.addEventListener('DOMContentLoaded', function () {
-    const inputElement = document.getElementById('floatingInput');
-    const labelElement = document.getElementById('labelText');
-
-    inputElement.addEventListener('input', function () {
-        labelElement.style.display = (inputElement.value === '') ? 'block' : 'none';
-    });
-});
-document.addEventListener('DOMContentLoaded', function () {
-    const passwordInput = document.getElementById('floatingPassword');
-    const passwordLabel = document.getElementById('labelPassword');
-
-    passwordInput.addEventListener('input', function () {
-        passwordLabel.style.display = (passwordInput.value === '') ? 'block' : 'none';
-    });
-});
