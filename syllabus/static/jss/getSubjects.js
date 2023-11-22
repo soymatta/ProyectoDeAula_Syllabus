dfdad22f211ee82050bd7c572f03746037d8c151
Ellery Ricaurte <elleryrica12@gmail.com>
2023-11-21 23:21:05 -0500

    getSubjetcs Finished

--- syllabus/static/jss/getSubjects.js
+++ syllabus/static/jss/getSubjects.js
@@ -1,20 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var tipoAsignatura = document.getElementById('idModalidadPresencial');
-    var tipo = tipoAsignatura.getAttribute('data-tipo');
-    if (tipo === 'presencial') {
-        console.log('hola')
-        tipoAsignatura.innerHTML += '<p>X</p>';
-    } else if (tipo === 'presencial-tic') {
-        console.log('hola')
-        tipoAsignatura = document.getElementById('idModalidadTIC');
-        if (tipoAsignatura) {
-            tipoAsignatura.innerHTML += '<p>X</p>';
+    var modalidadAsignatura = document.getElementById('idModalidadPresencial');
+    var modalidad = modalidadAsignatura.getAttribute('data-modalidad');
+    if (modalidad === 'presencial') {
+        modalidadAsignatura.innerHTML += '<p>X</p>';
+    } else if (modalidad === 'pre') {
+        modalidadAsignatura = document.getElementById('idModalidadTIC');
+        if (modalidadAsignatura) {
+            modalidadAsignatura.innerHTML += '<p>X</p>';
         }
-    } else if (tipo === 'virtual') {
-        console.log('hola')
-        tipoAsignatura = document.getElementById('idModalidadVirtual');
-        if (tipoAsignatura) {
-            tipoAsignatura.innerHTML += '<p>X</p>';
+    } else if (modalidad === 'virtual') {
+        modalidadAsignatura = document.getElementById('idModalidadVirtual');
+        if (modalidadAsignatura) {
+            modalidadAsignatura.innerHTML += '<p>X</p>';
         }
     }
+
+    var tipoAsignatura = document.getElementById('typeAsignatura')
+    var tipo = tipoAsignatura.getAttribute('data-tipo')
+
+    if(tipo === 'teorica' ){
+        tipoAsignatura = document.getElementById('idAsignaturaTeorica')
+        tipoAsignatura.innerHTML += '<p>X</p>';
+    }
+
+    if(tipo === 'teorico-practica'){
+        tipoAsignatura = document.getElementById('idAsignaturaTeoricoPractica')
+        tipoAsignatura.innerHTML += '<p>X</p>';
+    }
+
+    if(tipo ===  'practica'){
+        tipoAsignatura = document.getElementById('idAsignaturaPractica')
+    }
+
 });
---
+++ syllabus/static/jss/getVersions.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    changesUpdate = document.getElementById('changesTableBody');
+    changes = changesUpdate.getAttribute('data-changes');
+    console.log(changes)
+
+});
