845d800ff9fa2c9f849c4af799251f5d9ed23556
Ellery Ricaurte <elleryrica12@gmail.com>
2023-11-23 22:18:24 -0500

    Inserts SQL

--- documentation/syllabus.sql
+++ documentation/syllabus.sql
@@ -1,23 +1,11 @@
-INSERT INTO `contents_and_strategies` (`id`, `content_name`, `sub_content`, `strategies`, `syllabus_id`) VALUES
-(5, 'Introducción a la Ingeniería de Software', 'Definición y evolución.\r\nPrincipios y conceptos fundamentales.\r\nImportancia y aplicaciones.', 'Clases Teóricas y Prácticas:\r\n\r\nConferencias magistrales para la presentación de conceptos teóricos.\r\nDesarrollo práctico de proyectos en clase para aplicar los conocimientos adquiridos.', 2),
-(6, 'Conceptos Básicos de Cálculo', 'Funciones y límites.\r\nContinuidad y derivadas.\r\nAplicaciones de la derivada.', 'Clases Teóricas y Ejercicios Prácticos:\r\n\r\nLecciones teóricas con ejemplos prácticos.\r\nResolución de problemas en clase.', 4),
-(7, 'Introducción a IoT', 'Definición y evolución.\r\nPrincipios y tecnologías clave.\r\nAplicaciones en la vida cotidiana e industrial.', 'Proyectos Prácticos:\r\n\r\nDesarrollo de proyectos IoT desde cero.\r\nIntegración de sensores, actuadores y plataformas de desarrollo.', 3),
-(8, 'Ingeniería de Sistemas ', 'Jerarquía de la Ingeniería de Software \r\nIngeniería de procesos de negocios \r\nIngeniería del producto \r\nModelado del Sistema', 'Ejercicios en clase\r\nQuices,  \r\nTrabajo independiente del estudiante,  \r\nParticipación del estudiante,  \r\nExamen individual \r\nExposiciones ', 1);
-
-INSERT INTO `evaluations` (`id`, `first_percentage`, `description_first_percentage`, `second_percentage`, `description_second_percentage`, `third_percentage`, `description_third_percentage`, `syllabus_id`) VALUES
-(1, 30, 'Parciales y tareas', 30, 'Proyectos y tareas', 40, 'Parcial final', 4),
-(2, 30, 'Parciales y tareas', 30, 'Proyectos y tareas', 40, 'Parcial final', 1),
-(3, 30, 'Parciales y tareas', 30, 'Proyectos y tareas', 40, 'Parcial final', 2),
-(6, 100, 'Unico parcial', NULL, NULL, NULL, NULL, 3);
+INSERT INTO `subjects` (`id`, `name`, `modality`, `type`, `credits`, `semester`, `bibliography`) VALUES
+(1, 'Ingeniería del Software I', 'presencial', 'teorico-practica', 4, '1', NULL),
+(2, 'Ingeniería del Software II', 'presencial', 'teorico-practica', 4, '2', NULL),
+(3, 'Calculo I', 'presencial', 'teorico-practica', 4, '1', NULL),
+(4, 'Internet de las cosas', 'virtual', 'teorica', 2, '2', NULL);
 
 INSERT INTO `faculties` (`id`, `name`) VALUES
 (1, 'Ingeniería');
-
-INSERT INTO `faculties_subjects` (`id`, `faculties_id`, `subjects_id`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 1, 3),
-(4, 1, 4);
 
 INSERT INTO `formats` (`id`, `code`, `version`, `update_date`) VALUES
 (1, 'ST-DOC-01-E-01-F01 ', 1, '2021-07-26 00:00:00'),
@@ -26,19 +14,6 @@ INSERT INTO `formats` (`id`, `code`, `version`, `update_date`) VALUES
 INSERT INTO `programs` (`id`, `name`, `faculty_id`) VALUES
 (1, 'Ingeniería de sistemas', 1),
 (2, 'Ingeniería industrial', 1);
-
-INSERT INTO `programs_subjects` (`id`, `programs_id`, `subjects_id`) VALUES
-(1, 1, 3),
-(2, 1, 1),
-(3, 1, 2),
-(4, 1, 4),
-(5, 2, 3);
-
-INSERT INTO `subjects` (`id`, `name`, `modality`, `type`, `credits`, `semester`, `bibliography`) VALUES
-(1, 'Ingeniería del Software I', 'presencial', 'teorico-practica', 4, '1', NULL),
-(2, 'Ingeniería del Software II', 'presencial', 'teorico-practica', 4, '2', NULL),
-(3, 'Calculo I', 'presencial', 'teorico-practica', 4, '1', NULL),
-(4, 'Internet de las cosas', 'virtual', 'teorica', 2, '2', NULL);
 
 INSERT INTO `syllabi` (`id`, `cycle`, `component`, `justification`, `competences`, `learning_results`, `methodology`, `subject_id`, `faculty_id`, `format_id`, `program_id`) VALUES
 (1, 'PROFESIONAL', 'PROFESIONAL', 'Después de revisar y aplicar los conceptos previos de las fases de Ingeniería de Software (Análisis \r\ny Diseño de Sistemas), es indispensable para el estudiante comprender y desarrollar mecanismos \r\npara construir e implementar la aplicación que permitirá solucionar los problemas identificados \r\nen  la  fase  de  análisis  y  convertir  las  estructuras  definidas  en  la  fase  de  diseño  en  una  solución \r\npráctica, probada y documentada lista para ser usada por los usuarios.', 'Competencias básicas: Comunicar asertivamente las diferentes propuestas para dar solución a \r\nlas necesidades organizacionales que enfrenta el ingeniero de sistemas.', 'RA_4: \r\nComunica efectivamente en forma oral, gráfica y por escrito usando un lenguaje técnico.', 'TRABAJO PRESENCIAL \r\nLos conocimientos de la asignatura se adquieren a través del estudio razonado de todas las \r\nunidades didácticas de la asignatura, previa presentación del docente, ejemplos y ejercicios \r\nen clases, así como del material didáctico que se ponga a disposición de los estudiantes en \r\nla plataforma Moodle. \r\nTRABAJO INDEPENDIENTE \r\nLos estudiantes tendrán talleres y trabajos prácticos e investigativos sobre los temas vistos \r\nen clases que puedan realizar durante las horas independientes asignadas para la materia.', 2, 1, 2, 1),
@@ -51,6 +26,37 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `status`, `facul
 (2, 'Juan Martinez', 'teacher1@unilibre.edu.co', 'teacher', 'teacher', 1, 1),
 (3, 'Alba Romero', 'teacher2@unilibre.edu.co', 'teacher', 'teacher', 1, 1);
 
+
+INSERT INTO `versions` (`id`, `update_date`, `description`, `user_id`, `syllabus_id`) VALUES
+(1, '2023-11-24 01:51:24', 'Cambio 1', 3, 4),
+(2, '2023-11-01 19:53:18', 'Cambio 2', 2, 4),
+(3, '2023-11-24 01:51:24', 'Cambio 1', 2, 3);
+
+INSERT INTO `contents_and_strategies` (`id`, `content_name`, `sub_content`, `strategies`, `syllabus_id`) VALUES
+(5, 'Introducción a la Ingeniería de Software', 'Definición y evolución.\r\nPrincipios y conceptos fundamentales.\r\nImportancia y aplicaciones.', 'Clases Teóricas y Prácticas:\r\n\r\nConferencias magistrales para la presentación de conceptos teóricos.\r\nDesarrollo práctico de proyectos en clase para aplicar los conocimientos adquiridos.', 2),
+(6, 'Conceptos Básicos de Cálculo', 'Funciones y límites.\r\nContinuidad y derivadas.\r\nAplicaciones de la derivada.', 'Clases Teóricas y Ejercicios Prácticos:\r\n\r\nLecciones teóricas con ejemplos prácticos.\r\nResolución de problemas en clase.', 4),
+(7, 'Introducción a IoT', 'Definición y evolución.\r\nPrincipios y tecnologías clave.\r\nAplicaciones en la vida cotidiana e industrial.', 'Proyectos Prácticos:\r\n\r\nDesarrollo de proyectos IoT desde cero.\r\nIntegración de sensores, actuadores y plataformas de desarrollo.', 3),
+(8, 'Ingeniería de Sistemas ', 'Jerarquía de la Ingeniería de Software \r\nIngeniería de procesos de negocios \r\nIngeniería del producto \r\nModelado del Sistema', 'Ejercicios en clase\r\nQuices,  \r\nTrabajo independiente del estudiante,  \r\nParticipación del estudiante,  \r\nExamen individual \r\nExposiciones ', 1);
+
+INSERT INTO `evaluations` (`id`, `first_percentage`, `description_first_percentage`, `second_percentage`, `description_second_percentage`, `third_percentage`, `description_third_percentage`, `syllabus_id`) VALUES
+(1, 30, 'Parciales y tareas', 30, 'Proyectos y tareas', 40, 'Parcial final', 4),
+(2, 30, 'Parciales y tareas', 30, 'Proyectos y tareas', 40, 'Parcial final', 1),
+(3, 30, 'Parciales y tareas', 30, 'Proyectos y tareas', 40, 'Parcial final', 2),
+(6, 100, 'Unico parcial', NULL, NULL, NULL, NULL, 3);
+
+INSERT INTO `faculties_subjects` (`id`, `faculties_id`, `subjects_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 1, 4);
+
+INSERT INTO `programs_subjects` (`id`, `programs_id`, `subjects_id`) VALUES
+(1, 1, 3),
+(2, 1, 1),
+(3, 1, 2),
+(4, 1, 4),
+(5, 2, 3);
+
 INSERT INTO `users_programs` (`id`, `users_id`, `programs_id`) VALUES
 (1, 3, 2),
 (2, 3, 1),
@@ -62,8 +68,3 @@ INSERT INTO `users_subjects` (`id`, `users_id`, `subjects_id`) VALUES
 (3, 2, 2),
 (4, 2, 4),
 (5, 3, 4);
-
-INSERT INTO `versions` (`id`, `update_date`, `description`, `user_id`, `syllabus_id`) VALUES
-(1, '2023-11-24 01:51:24', 'Cambio 1', 3, 4),
-(2, '2023-11-01 19:53:18', 'Cambio 2', 2, 4),
-(3, '2023-11-24 01:51:24', 'Cambio 1', 2, 3);
--- syllabus/static/jss/bloques.js
+++ syllabus/static/jss/bloques.js
@@ -1,60 +1,75 @@
-// Contenido del script.js
-document.addEventListener('DOMContentLoaded', function () {
-    var blockCounter = 1;  // Contador para identificar de manera única cada bloque
+document.addEventListener("DOMContentLoaded", function () {
+    var blockCounter = 1; // Contador para identificar de manera única cada bloque
     var listaBloques = [];
-
+  
     // Función para crear un nuevo bloque y su correspondiente ítem
     function createNewBlock(content, strategy) {
-
-        var accordionBody = document.querySelector('.bloquesCreados');
-
-        // Crear nuevo bloque
-        var newBlock = document.createElement('div');
-        var blockId = 'block' + blockCounter;  // Crear un ID único para el bloque
-        newBlock.id = blockId;
-        newBlock.classList.add('block');
-        newBlock.innerHTML = `
-            <h2>Bloque ${blockCounter}</h2>
-            <div class="markdown-content" id="${blockId}-content"></div> <!-- Contenedor para contenido Markdown -->
-            <div class="markdown-strategy" id="${blockId}-strategy"></div> <!-- Contenedor para estrategias Markdown -->
-        `;
-
-        // Añadir bloque al contenedor
-        accordionBody.appendChild(newBlock);
-
-        // Crear correspondiente ítem con botones de configuración
-        var newItem = document.createElement('div');
-        newItem.classList.add('item');
-        newItem.dataset.blockId = blockId;  // Añadir un atributo de datos para almacenar el ID del bloque
-        newItem.innerHTML = `
-            <span>Bloque ${blockCounter}</span>
-            <button class="config-button" onclick="editBlock('${blockId}')">Editar</button>
-            <button class="config-button" onclick="deleteBlock('${blockId}')">Eliminar</button>
-        `;
-
-        // Añadir ítem al contenedor
-        document.getElementById('blockItems').appendChild(newItem);
-
-        // Incrementar el contador para el próximo bloque
-        blockCounter++;
-        listaBloques.add(newBlock);
-
-        // Actualizar el contenido del bloque con información Markdown y en los divs específicos
-        
-
-        // Actualizar los divs program-content y strategies con la información de los bloques
-        
+      var bloques = document.querySelector(".bloquesCreados");
+  
+      // Crear nuevo bloque
+      var newBlock = document.createElement("div");
+      var blockId = blockCounter; // Crear un ID único para el bloque
+      newBlock.id = blockId;
+      newBlock.classList.add("block");
+      newBlock.innerHTML = `
+          <h2>Bloque ${blockCounter}</h2>
+          <div class="markdown-content" id="${blockId}-content"></div> <!-- Contenedor para contenido Markdown -->
+          <div class="markdown-strategy" id="${blockId}-strategy"></div> <!-- Contenedor para estrategias Markdown -->
+      `;
+  
+      // Añadir bloque al contenedor
+      bloques.appendChild(newBlock);
+  
+      // Crear correspondiente ítem con botones de configuración
+      var newItem = document.createElement("div");
+      newItem.classList.add("item");
+      newItem.dataset.blockId = blockId; // Añadir un atributo de datos para almacenar el ID del bloque
+      newItem.innerHTML = `
+          <span>Bloque ${blockCounter}</span>
+          <button class="config-button" onclick="editBlock('${blockId}')">Editar</button>
+          <button class="config-button" onclick="deleteBlock('${blockId}')">Eliminar</button>
+      `;
+  
+      // Añadir ítem al contenedor
+      document.getElementById("blockItems").appendChild(newItem);
+  
+      // Incrementar el contador para el próximo bloque
+      blockCounter++;
+      listaBloques.push(newBlock);
+  
+      // Actualizar el contenido del bloque con información Markdown y en los divs específicos
+      markDownBlock();
+  
+      // Actualizar los divs program-content y strategies con la información de los bloques
     }
-
-    function markDownBlock(){
-        
-        if(listaBloques.length == 0){
-            console.log("no hay bloques");
-        } else{
-            for(var i = 0; i < listaBloques.length; i++){
-                
-            }
-        }
-    }   
-
-})
+  
+    function markDownBlock() {
+      for (var i = 0; i < listaBloques.length; i++) {
+        var block = listaBloques[i];
+  
+        /* contenido de los bloques */
+        var textContent = block.querySelector(`#${i}-content`).value;
+        var textLearning = block.querySelector(`#${i}-strategy`).value;
+  
+        var converter = new showdown.Converter();
+  
+        /* contenido de los bloques en html*/
+        var htmlContent = converter.makeHtml(textContent);
+        var htmlLearning = converter.makeHtml(textLearning);
+  
+        /* targets */
+        var contentMarkdownBlock = document.createElement('div');
+        var learningMarkdownBlock = document.createElement('div');
+  
+        contentMarkdownBlock.innerHTML = htmlContent;
+        learningMarkdownBlock.innerHTML = htmlLearning;
+  
+        var targetContent = document.getElementById('program-content');
+        var targetLearning = document.getElementById('strategies');
+  
+        targetContent.appendChild(contentMarkdownBlock);
+        targetLearning.appendChild(learningMarkdownBlock);
+      }
+    }
+  });
+  
